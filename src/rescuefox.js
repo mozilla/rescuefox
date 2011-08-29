a113b64ea65f8b8e91ca9a692e2ad59e15e537fd
Bobby Richter <secretrobotron@gmail.com>
2011-08-29 19:35:03 -0400

    1. Asteroid assets. 2. Beginning of work to tie in asteroids.

--- src/rescuefox.js
+++ src/rescuefox.js
@@ -10,7 +10,12 @@ var Game = function( options ) {
     Ship.prototype = new engine.Entity();
     Ship.constructor = Ship;
 
+    var asteroidMesh = CubicVR.loadCollada("../assets/asteroids/asteroids1.dae","../assets/asteroids/");
     var Asteroid = function( options ) {
+        var model = new engine.component.Model({
+            mesh: asteroidMesh
+        });
+        this.addComponent( model );
     };
     Asteroid.prototype = new engine.Entity();
     Asteroid.constructor = Asteroid;
@@ -22,10 +27,19 @@ var Game = function( options ) {
 
     var scene = new engine.Scene();
     scene.graphics.setSkyBox(new engine.graphics.CubicVR.SkyBox({
-      texture: "../assets/space_skybox.jpg"}));
+      texture: "../assets/space_skybox.jpg"
+    }));
 
     this.run = function() {
         engine.run();
+        for ( var i=0; i<10; ++i ) {
+            var asteroid = new Asteroid();
+            asteroid.spatial.position[0] = -250 + Math.random()*500;
+            asteroid.spatial.position[1] = -2 + Math.random()*4;
+            asteroid.spatial.position[2] = -250 + Math.random()*500;
+            asteroid.setParent( scene );
+        }
+        window.foo = scene;
     };
 };
 
@@ -33,7 +47,7 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
     paladin.create( { debug: true },
         function( engineInstance ) {
             var game = new Game( { engine: engineInstance } );
-            game.run();   
+            game.run();
         }
     );
 }, false );
